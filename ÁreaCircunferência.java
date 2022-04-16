2547d1b4335a687a9858791289ce649cb1406899
Caíque Villela <92598305+analistacaique@users.noreply.github.com>
2022-04-16 18:38:42 -0300

    Add files via upload

---
+++ ÁreaCircunferência.java
@@ -0,0 +1,13 @@
+package Fundamentos;
+
+public class ÁreaCircunferência {
+
+	public static void main(String[] args) {
+		double raio = 3.2;
+		double pi = 3.14159;
+		double area = pi * raio * raio;
+		
+		System.out.println(area);
+		
+	}
+}
